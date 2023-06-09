9836f172621153a88f8abaed75d847fd50d45a37
agmui <anthonynmui@gmail.com>
2023-06-08 22:37:40 -0400

    updated to catppucine

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,6 +8,6 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar example --config=$HOME/dotFiles/config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
